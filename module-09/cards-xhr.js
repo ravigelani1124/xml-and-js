ff0c2bd241ec6b529099ee88fe84388a16921edc
Ravi Gelani <ravi.gelanee@gmail.com>
2023-03-22 12:21:25 -0400

    Module 09 Activty 1 and 2

--- module-09/cards-xhr.js
+++ module-09/cards-xhr.js
@@ -1,21 +1,24 @@
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    displayData(this.responseXML);
-  }
-};
-xhttp.open("GET", "cards.xml");
-xhttp.send();
+const xhr = (url, method = 'GET') =>
+	new Promise((resolve) => {
+		const xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				resolve(this.responseXML);
+			}
+		};
+		xhttp.open(method, url);
+		xhttp.send();
+	});
 
 function stringToNode(html) {
-  const template = document.createElement(`template`);
-  html = html.trim();
-  template.innerHTML = html;
-  return template.content.firstChild;
+	const template = document.createElement(`template`);
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
 }
 
 function createCard(card) {
-  const content = `<li>
+	const content = `<li>
   <article>
     <p>${card.type}</p>
     <h2>${card.number}</h2>
@@ -25,35 +28,38 @@ function createCard(card) {
   </article>
 </li>`;
 
-  return stringToNode(content);
+	return stringToNode(content);
 }
 
 function parseCard(xml) {
-  const number = xml.getElementsByTagName(`number`)[0].childNodes[0].nodeValue;
-  const type = xml.getElementsByTagName(`type`)[0].childNodes[0].nodeValue;
-  const currency =
-    xml.getElementsByTagName(`currency`)[0].childNodes[0].nodeValue;
-  const balance =
-    xml.getElementsByTagName(`balance`)[0].childNodes[0].nodeValue;
+	const number =
+		xml.getElementsByTagName(`number`)[0].childNodes[0].nodeValue;
+	const type = xml.getElementsByTagName(`type`)[0].childNodes[0].nodeValue;
+	const currency =
+		xml.getElementsByTagName(`currency`)[0].childNodes[0].nodeValue;
+	const balance =
+		xml.getElementsByTagName(`balance`)[0].childNodes[0].nodeValue;
 
-  return {
-    number,
-    type,
-    currency,
-    balance,
-  };
+	return {
+		number,
+		type,
+		currency,
+		balance,
+	};
 }
 
 function displayData(xmlDoc) {
-  const cards = xmlDoc.getElementsByTagName(`record`);
+	const cards = xmlDoc.getElementsByTagName(`record`);
 
-  const list = document.getElementById(`cards`);
+	const list = document.getElementById(`cards`);
 
-  for (let index = 0; index < cards.length; index++) {
-    const element = cards[index];
-    const parsedCard = parseCard(element);
+	for (let index = 0; index < cards.length; index++) {
+		const element = cards[index];
+		const parsedCard = parseCard(element);
 
-    const cardElement = createCard(parsedCard);
-    list.appendChild(cardElement);
-  }
+		const cardElement = createCard(parsedCard);
+		list.appendChild(cardElement);
+	}
 }
+
+xhr('cards.xml').then((data) => displayData(data));
